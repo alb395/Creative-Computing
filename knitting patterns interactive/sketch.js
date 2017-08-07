baf4c019fb86994532466af68dae5994507d4f89
alb395 <alb395@nyu.edu>
2017-08-07 10:50:51 -0400

    working with speed and gauge and go button

--- knitting patterns interactive/sketch.js
+++ knitting patterns interactive/sketch.js
@@ -1,6 +1,6 @@
 var x = 0;
 var y = 0;
-var speed;
+var speed = 0;
 var gauge; // size of the box
 
 var gaugeButton, gaugeInput;
@@ -10,30 +10,26 @@ function setup() {
 	createCanvas(400,400);
 	background(97,63,117);	
 
+	var speedLabel = createP('speed: ');
+	speedLabel.position(width, 0);
+
 	// creates an input box and sets position
 	    speedInput = createInput();
-	    speedInput.position(width, 0);
-	// creates a button and sets position
-	    // speedButton = createButton('save speed');
-	    // speedButton.position(width, speedInput.height);
-	// button callback function
-		// speedButton.mousePressed(saveSpeed);
+	    speedInput.position(width, speedLabel.height + 20);
 
-		// creates an input box and sets position
+	var gaugeLabel = createP('gauge: ');
+	gaugeLabel.position(width, 40);
+
+	// creates an input box and sets position
 	    gaugeInput = createInput();
-	    gaugeInput.position(width, 50);
-	// creates a button and sets position
-	//     gaugeButton = createButton('save gauge');
-	//     gaugeButton.position(width, 50 + gaugeInput.height); 
-	// button callback function
-		// gaugeButton.mousePressed(saveGauge);
+	    gaugeInput.position(width, 80);
 
 	// go button
 		goButton = createButton('Go!');
-		goButton.position(width, 100);
+		goButton.position(width, 120);
 		goButton.mousePressed(runPattern);
 
-    noLoop(); // don't run until last input is submitted
+    noLoop(); // don't run until Go button is pressed
 }
 
 function draw() {
@@ -54,22 +50,24 @@ function draw() {
 	
 	if(mouseIsPressed){
 		fill(255);
-		rect(x, y, gauge, gauge);
+		rect(x, y, 1.5*gauge, gauge);
 	}
 }
 
 function saveSpeed() {
 	// save the input in a variable
 	var userSpeed = speedInput.value();
-	speedInput.value('');
+	// speedInput.value('');
 	console.log("selected speed: " + userSpeed);
 	speed = Number(userSpeed);
+
+	
 }
 
 function saveGauge() {
     // save the input in a variable
   	var userGauge = gaugeInput.value();
-    gaugeInput.value(''); //clears saved text from the input box
+    // gaugeInput.value(''); //clears saved text from the input box
     console.log("selected gauge: " + userGauge);
     gauge = Number(userGauge);
 }
@@ -78,4 +76,6 @@ function runPattern(){
 	saveGauge();
 	saveSpeed();
 	loop();
+
+
 }
