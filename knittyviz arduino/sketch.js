2acd9c25d2d5b498c56ff7337b92752e954276fe
alb395 <alb395@nyu.edu>
2017-08-07 23:47:23 -0400

    Added Arduino connection with digital switch
    Attempting to incorporate serial communication using my knitting
    needles as a digital switch. It’s not working right yet.

--- knittyviz arduino/sketch.js
+++ knittyviz arduino/sketch.js
@@ -2,6 +2,9 @@ var serial;// variable to hold the serialport library
 var portName='/dev/cu.usbmodem1421';// use your serial port name
 var inByte=0;
 
+var needlesTouching = 0;
+var actualThing = 0;
+
 var x = 0;
 var y = 0;
 var speed = 0;
@@ -15,6 +18,16 @@ var resetButton;
 function setup() {
 	createCanvas(400,420);
 	background(97,63,117);	
+
+// serial communication info
+	serial=new p5.SerialPort(); // make a new instance of the serialport library
+    //serial.on('list', printList);    // set a callback function for the serialport list event
+    serial.on('connected',serverConnected);// callback for connecting to the server
+    serial.on('open',portOpen);// callback for the port opening
+    serial.on('data',serialEvent);// callback for when new data arrives
+    serial.on('error',serialError);// callback for errors
+    serial.on('close',portClose);// callback for the port closing
+    serial.open(portName);// open a serial port
 
  // creates an input slider and sets position
     speedSlider = createSlider(1,20,5,1);
@@ -69,10 +82,15 @@ function draw() {
 		noLoop();
 	}
 	
-	if(mouseIsPressed){
+	if(needlesTouching == 48){ // why isn't this only applying when the value is 48
 		fill(255);
 		rect(x, y, gauge, gauge);
 	}
+
+	// if(mouseIsPressed){
+	// 	fill(255);
+	// 	rect(x, y, gauge, gauge);
+	// }
 
 	fill(255);
 	rect(0, height-20, width, 20);
@@ -84,7 +102,7 @@ function draw() {
 function saveGauge() {
     // save the input in a variable
   	var userGauge = gaugeSlider.value();
-    // gaugeInput.value(''); //clears saved text from the input box
+    // gaugeInput.value(''); //clears entered text from the input box
     console.log("selected gauge: " + userGauge);
     gauge = Number(userGauge);
 }
@@ -109,4 +127,34 @@ function refresh(){
 	setup();
 	x = 0;
 	y = 0;
+}
+
+function serverConnected(){
+    console.log('connected to server.');
+}
+
+function portOpen(){
+     console.log('the serial port opened.');
+}
+
+function serialEvent(){
+    // needlesTouching = serial.readStringUntil('\r\n'); // hold info in the buffer until you see the control characters, and store that in our inString variable
+    // needlesTouching = needlesTouching.trim();
+	// if (needlesTouching.length > 0) { //if we have any information, use it!
+	//     inByte = Number(needlesTouching);
+	//     print(inByte);
+	//   }
+
+	needlesTouching = Number(serial.read());
+
+    console.log("serial read is: " + serial.read());
+    console.log("needlesTouching variable is: " + needlesTouching);
+}
+
+function serialError(err){
+     console.log('Something went wrong with the serial port. '+ err);
+}
+
+function portClose(){
+     console.log('The serial port closed.');
 }
